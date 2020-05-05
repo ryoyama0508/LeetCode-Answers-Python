0167e770167861bda785426a16290f840655fcb9
山本凌 <ryo.dr.dre0508@gmail.com>
2020-05-06 03:20:33 +0900

    add

---
+++ PythonAns/climbingStairs.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return self.climbRecursive(0, n)
+
+    def climbRecursive(self, current, goal):
+        if current > goal:
+            return 0
+        elif current == goal:
+            return 1
+
+        return self.climbRecursive(current + 1, goal) + self.climbRecursive(current + 2, goal)
+        # time limit exceeded
+
+    def climbLoop(self, n):
+        d = []
+        [0] * n
+        d[0] = 0
+        d[1] = 1
+        i = 2
+        for i, _ in enumerate(d):
+            d[i] = d[i-1] + d[i - 2]
+            i += 1
+            if i > n:
+                break
+
+        return d[n]
