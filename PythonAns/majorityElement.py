e9d5a6f9f858cb70ed8d5711a21900bb1bfe6fce
山本凌 <ryo.dr.dre0508@gmail.com>
2020-05-15 19:48:31 +0900

    add

---
+++ PythonAns/majorityElement.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dic = {}
+        for num in nums:
+            if num in dic:
+                dic[num] = dic[num] + 1
+            else:
+                dic[num] = 1
+
+        for i, v in dic.items():
+            if v > (len(nums)/2):
+                return i
