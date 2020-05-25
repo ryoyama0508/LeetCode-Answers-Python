e6179636c846d17cc8fe3c53d5981e163495a11e
山本凌 <ryo.dr.dre0508@gmail.com>
2020-05-26 01:10:18 +0900

    my idea solution

--- PythonAns/maxSubarray.py
+++ PythonAns/maxSubarray.py
@@ -10,3 +10,19 @@ class Solution(object):
             maxSum = max(maxSum, curSum)
 
         return maxSum
+
+    def maxSubArray2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        curr = prev = nums[0]
+        i = 0
+        for num in nums:
+            if i == 0:
+                i += 1
+                continue
+            prev = max(num, prev + num)
+            curr = max(curr, prev)
+
+        return curr
