a407971e4f53af793fe5103008a3e4c3e5e3b559
山本凌 <ryo.dr.dre0508@gmail.com>
2020-05-26 22:34:27 +0900

    add

--- PythonAns/test.py
+++ PythonAns/test.py
@@ -4,6 +4,28 @@ class ListNode(object):
         self.val = val
         self.next = next
 
+    def quickSort(self, nums):
+        if len(nums) == 1:
+            return nums
+        povit = 0
+
+        if len(nums) == 2 and nums[0] == nums[1]:
+            return nums
+        if nums[0] < nums[1]:
+            povit = nums[1]
+        else:
+            povit = nums[0]
+
+        newNums1 = []
+        newNums2 = []
+        for num in nums:
+            if num < povit:
+                newNums1.append(num)
+            else:
+                newNums2.append(num)
+
+        return self.quickSort(newNums1)+self.quickSort(newNums2)
+
 
 node = ListNode(0)
 print(node)
