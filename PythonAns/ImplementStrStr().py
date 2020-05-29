d05f82a79d7342a9afcd6b6dd1b24499305a2285
山本凌 <ryo.dr.dre0508@gmail.com>
2020-05-30 02:48:40 +0900

    add

---
+++ PythonAns/ImplementStrStr().py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        l1, l2 = len(haystack), len(needle)
+        for i in range(l1 - l2 + 1):
+            if needle == haystack[i:i+l2]:
+                return i
+        return -1
