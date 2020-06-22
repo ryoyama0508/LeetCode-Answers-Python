d13e6fd9f3a69885745eded3ae33e01f427db42c
山本凌 <ryo.dr.dre0508@gmail.com>
2020-06-23 00:39:39 +0900

    add

---
+++ PythonAns/containDuplicate2.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        dic = {}
+        for i in range(0, len(nums)):
+            if nums[i] in dic:
+                if i - dic[nums[i]] <= k:
+                    return True
+            dic[nums[i]] = i
+
+        return False
