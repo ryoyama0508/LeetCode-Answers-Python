d8e22a1f46e7a95392cbafbfbda1527ae3597ff1
山本凌 <ryo.dr.dre0508@gmail.com>
2020-07-24 19:06:25 +0900

    add

--- PythonAns/maximalSquare.py
+++ PythonAns/maximalSquare.py
@@ -9,3 +9,29 @@ class Solution(object):
                                      [j], copy[i][j-1])+1
                     ret = max(ret, copy[i][j])
         return ret**2
+
+    def maximalSquare1(self, matrix):
+        ret = 0
+        for i in range(0, len(matrix)):
+            for j in range(0, len(matrix[0])):
+                if matrix[i][j] == '1':
+                    ret = max(self.diagonal(matrix, i, j, 0), ret)
+        return ret**2
+
+    def diagonal(self, matrix, i, j, long):
+        if i >= len(matrix) or j >= len(matrix[0]) or matrix[i][j] != '1':
+            return long
+        right = self.right(matrix, i, j+1, long+1)
+        diagonal = self.diagonal(matrix, i+1, j+1, long+1)
+        down = self.down(matrix, i+1, j, long+1)
+        return min(right, diagonal, down)
+
+    def right(self, matrix, i, j, long):
+        if i >= len(matrix) or j >= len(matrix[0]) or matrix[i][j] != '1':
+            return long
+        return self.right(matrix, i, j+1, long+1)
+
+    def down(self, matrix, i, j, long):
+        if i >= len(matrix) or j >= len(matrix[0]) or matrix[i][j] != '1':
+            return long
+        return self.down(matrix, i+1, j, long+1)
