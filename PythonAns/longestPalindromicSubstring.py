061cc2208ec9dcf36aaa063f5ee7d887bfa536b2
山本凌 <ryo.dr.dre0508@gmail.com>
2020-08-31 15:49:49 +0900

    add

--- PythonAns/longestPalindromicSubstring.py
+++ PythonAns/longestPalindromicSubstring.py
@@ -1,25 +1,45 @@
 class Solution(object):
     def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        if s == '':
-            return ''
-        start = end = 0
-        for i in range(0, len(s)):
-            lenP1 = self.center(s, i, i)
-            lenP2 = self.center(s, i, i+1)
-            le = max(lenP1, lenP2)
-            if le > end - start:
-                start = i - ((le - 1)/2)
-                end = i + (le / 2)
+        res = ""
+        for i in range(len(s)):
+            print(i, "idx")
+            # odd case, like "aba"
+            tmp = self.helper(s, i, i)
+            print(tmp)
+            if len(tmp) > len(res):
+                res = tmp
+            # even case, like "abba"
+            tmp = self.helper(s, i, i+1)
+            print(tmp)
+            if len(tmp) > len(res):
+                res = tmp
+        return res
 
-        return s[start: end + 1]
-
-    def center(self, s, l, r):
+    # get the longest palindrome, l, r are the middle indexes
+    # from inner to outer
+    def helper(self, s, l, r):
         while l >= 0 and r < len(s) and s[l] == s[r]:
             l -= 1
             r += 1
+        print(l, r, "helper")
+        return s[l+1:r]
 
-        return r - l - 1
+
+obj = Solution()
+print(obj.longestPalindrome("cbbd"))
+
+
+class Solution2:
+    def longestPalindrome2(self, s: str) -> str:
+        dp = [[0 for i in range(len(s))] for i in range(len(s))]
+        ret = ''
+        for i in range(len(s)-1, -1, -1):
+            for j in range(i, len(s)):
+                if len(s)-j < len(ret):
+                    break
+                if (i - j == 0) or (j - i == 1 and s[i] == s[j]) or (dp[i+1][j-1] == 1 and s[i] == s[j]):
+                    feas_ans = s[i:j+1]
+                    if len(feas_ans) > len(ret):
+                        ret = s[i:j+1]
+                    dp[i][j] = 1
+        return ret
--- PythonAns/palindromicSubstrings.py
+++ PythonAns/palindromicSubstrings.py
@@ -1,2 +1,15 @@
-class Solution:
+class Solution(object):
     def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        res = 0
+        for i in range(n-1, -1, -1):
+            for j in range(i, n):
+                if (i - j == 0) or (j - i == 1 and s[i] == s[j]) or (dp[i+1][j-1] == 1 and s[i] == s[j]):
+                    res += 1
+                    dp[i][j] = 1
+        return res
+
+
+obj = Solution()
+print(obj.countSubstrings("abc"))
