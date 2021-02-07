67159342828771cb56e98afd6fae7ef38d528d26
山本凌 <ryo.dr.dre0508@gmail.com>
2021-02-08 00:55:49 +0900

    minstack

--- PythonAns/minStack.py
+++ PythonAns/minStack.py
@@ -1,42 +1,71 @@
 class MinStack(object):
+
     def __init__(self):
-        self.val = None
-        self.next = None
-        self.last = None
-        self.min = float("inf")
+        self.q = []
 
+# @param x, an integer
+# @return an integer
     def push(self, x):
-        self.val = x
-        if self.last:
-            self.min = min(self.last.min, x)
-        else:
-            self.min = x
+        curMin = self.getMin()
+        if curMin == None or x < curMin:
+            curMin = x
+        self.q.append((x, curMin))
 
-        self.next = MinStack()
-        temp = self
-        self = self.next
-        self.last = temp
-
+# @return nothing
     def pop(self):
-        self = self.last
-        self = MinStack()
+        self.q.pop()
+
+
+# @return an integer
 
     def top(self):
-        return self.val
+        if len(self.q) == 0:
+            return None
+        else:
+            return self.q[-1][0]
+
+
+# @return an integer
 
     def getMin(self):
-        return self.min
+        if len(self.q) == 0:
+            return None
+        else:
+            return self.q[-1][1]
+
+
+class MinStack2(object):
+    def __init__(self):
+        self.stack = []
+        self.stackMin = []
+
+    def push(self, x):
+        self.stack.append(x)
+        self.stackMin.append(x)
+        self.stackMin.sort()
+
+    def pop(self):
+        for i in range(len(self.stackMin)):
+            if self.stackMin[i] == self.stack[-1]:
+                self.stackMin = self.stackMin[:i]+self.stackMin[i+1:]
+                break
+
+        self.stack = self.stack[:-1]
+
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.stackMin[0]
 
 
 # Your MinStack object will be instantiated and called as such:
-obj = MinStack()
+obj = MinStack2()
 
 inp = [-2, 0, -3]
 for i in inp:
-    print(obj.val)
     obj.push(i)
-    print(obj.last)
 
-obj.pop()
-print('top', obj.top())
-print('get min', obj.getMin())
+print(obj.pop())
+print(obj.top())
+print(obj.getMin())
