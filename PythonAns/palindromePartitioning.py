bbf31670495d2786a8b67c770b17eb3546b4b9f1
山本凌 <ryo.dr.dre0508@gmail.com>
2021-03-10 22:43:56 +0900

    add

--- PythonAns/palindromePartitioning.py
+++ PythonAns/palindromePartitioning.py
@@ -16,8 +16,35 @@ class Solution:
         helper(s, [])
         return ret
 
+    def partition2(self, s):
+        memo = {}
 
-l = []
-print([] + ["b", "c"])
-l.append(["b", "c"])
-print(l)
+        def palindrome(s):
+            return s == s[::-1]
+
+        def helper(rest_str):
+            memo[rest_str] = []
+            if len(rest_str) == 1:
+                memo[rest_str] = [[rest_str]]
+                return
+
+            for i in range(1, len(rest_str) + 1):
+                this_time_str = rest_str[:i]
+                taken_str = rest_str[i:]
+
+                if palindrome(this_time_str):
+
+                    if taken_str in memo:  # in memo
+                        for l in memo[taken_str]:  # loop memo list value
+                            memo[rest_str].append([this_time_str] + l)
+
+                    else:
+                        if taken_str != "":
+                            helper(taken_str)
+                            for l in memo[taken_str]:
+                                memo[rest_str].append([this_time_str] + l)
+                        else:
+                            memo[rest_str].append([this_time_str])
+
+        helper(s)
+        return memo[s]
